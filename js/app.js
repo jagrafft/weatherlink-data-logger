e284129c4ba8e83665b2c365474f3c6c541938a9
jason a. grafft <jagrafft@users.noreply.github.com>
2020-06-06 16:48:19 -0500

    Refactored HTTP call

--- js/app.js
+++ js/app.js
@@ -1,11 +1,7 @@
 // Ideas //
 // - Refactor to use xstream.js for writing to a Kafka service
 
-// Make values in `.env` available to program (`process.env.<VAR_NAME>`)
-require("dotenv").config()
-
 const Pool = require("pg")  // postgres connection pool
-const request = require("request")  // API for HTTP requests
 
 /**
  * "Instructions" for connecting to postgres instance
@@ -19,6 +15,7 @@ const pool = new Pool({
     port: process.env.POSTGRES_PORT
 })
 
+
 /**
  * URL of WeatherLink station endpoint
  * @const {String}
@@ -26,25 +23,8 @@ const pool = new Pool({
 const weatherlink_url = process.env.WEATHERLINK_URL
 
 /*
-`setInterval` executes a function every $n$ milliseconds (see the end of 
-    the function block). Here, we are using a Lambda function, which is _only_
-    available to `setInterval` because it is not stored in memory outside the
-    scope of that function (`Pool`, `request`, `pool`, and `weatherlink_url` 
-    are).
+`setInterval` executes a function every $n$ milliseconds
 */
 setInterval(() => {
-        request.get({
-                url: weatherlink_url,
-                json: true,
-                headers: {"User-Agent": "request"}
-        }, (err, res, data) => {
-                if (err) {
-                    console.error(err)
-                } else if (res.statusCode !== 200) {
-                    console.log(`Status: ${res.statusCode}`)
-                } else {
-                    // Chain of futures...
-                    console.log(data)
-                }
-        })
+    data_request(weatherlink_url)
 }, 1000) // Refresh interval, in milliseconds
--- js/utils.js
+++ js/utils.js
@@ -1,16 +1,40 @@
 // Well-built library for the `Future` type, used to make
 // operations asynchronous
 const {Future} = require("fluture")
+const request = require("request")  // API for HTTP requests
+
+// TODO Wrap in Future
+/**
+ * Request data from `url`
+ * @param {String} url URL to request from
+ * @returns {Fluture.<Future>}
+ */
+data_request = (url) => {
+  request.get({
+    url: weatherlink_url,
+    json: true,
+    headers: {"User-Agent": "request"}
+  }, (err, res, data) => {
+    if (err) {
+        console.error(err)
+    } else if (res.statusCode !== 200) {
+        console.log(`Status: ${res.statusCode}`)
+    } else {
+        // Chain of futures...
+        console.log(data)
+    }
+  })
+}
 
 /**
- * Check if `entity` exists in postgres `table` in `db` and return its
+ * Check if `entity` exists in postgres `table` in `pool` then return its
  * primary key or insert entity on fail
- * @param {String} db Database to check
+ * @param {} pool PostgreSQL pool
  * @param {String} table Table to search
  * @param {String|Number} entity Value to search for
  * @returns {Fluture.<Future>} `id` || Failure
  */
-export const entityExistsOrInsert = (db, table, entity) => {
+export const entityExistsOrInsert = (pool, table, entity) => {
     
 }
 
@@ -23,8 +47,8 @@ export const mkInsertString = () => {
 }
 
 /**
- * Write `cmd` to postgres `table` in `db`
- * @param {String} db Database to write to
+ * Write `cmd` to postgres `table` in `pool`
+ * @param {} pool PostgreSQL pool
  * @param {String} table Table to write to
  * @param {String} cmd Command string
  * @returns {Fluture.<Future>}
@@ -35,8 +59,8 @@ export const pgWrite = (db, table, cmd) => {
 
 // TODO Refactor to build table?
 /**
- * Check if postgres `table` exists in `db`
- * @param {String} db Database to check
+ * Check if postgres `table` exists in `pool`
+ * @param {} pool PostgreSQL pool
  * @param {String} table Table to check for
  * @returns {Fluture.<Future>}
  */
